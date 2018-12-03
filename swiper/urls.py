72a283ea7f3a2fbc0f812ebf06cf0e949a81f1ae
muzili <2484167983@qq.com>
2018-12-03 16:21:56 +0800

    p

--- swiper/urls.py
+++ swiper/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 
 from user import api as user_api
+from social import api as social_api
 
 urlpatterns = [
     url(r'^api/user/verify$', user_api.get_verify_code),
@@ -24,5 +25,10 @@ urlpatterns = [
     url(r'^api/user/profile/modify$', user_api.modify_profile),
     url(r'^api/user/avatar$', user_api.upload_avatar),
 
-
+    url(r'^api/social/users', social_api.get_users),
+    url(r'^api/social/like', social_api.like),
+    url(r'^api/social/superlike', social_api.superlike),
+    url(r'^api/social/dislike', social_api.dislike),
+    url(r'^api/social/rewind', social_api.rewind),
+    url(r'^api/social/friends', social_api.friends),
 ]
